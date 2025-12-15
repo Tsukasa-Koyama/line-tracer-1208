ca8bb17e253451c22fa752b5bc43dec756d4393b
Tsukasa-Koyama <tkr0rempe1230@gmail.com>
2025-12-15 10:58:29 +0900

    旋回指示を追加（両方白）

--- main.ts
+++ main.ts
@@ -4,13 +4,7 @@ while (true) {
     } else if (Tinybit.Line_Sensor(Tinybit.enPos.RightState, Tinybit.enLineState.Black)) {
         Tinybit.car_sport(65, 65)
     } else if (Tinybit.Line_Sensor(Tinybit.enPos.LeftState, Tinybit.enLineState.Black)) {
-        basic.showLeds(`
-            . . # . .
-            . . # . .
-            . . # . .
-            . . . . .
-            . . # . .
-            `)
+        Tinybit.car_sport(0, 62)
     } else if (Tinybit.Line_Sensor(Tinybit.enPos.LeftState, Tinybit.enLineState.White) && Tinybit.Line_Sensor(Tinybit.enPos.RightState, Tinybit.enLineState.White)) {
         Tinybit.car_sport(0, 62)
     } else {
