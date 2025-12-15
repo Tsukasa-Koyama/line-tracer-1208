8a5fe51079bc568a95d47b061e95dc18f663ee9f
Tsukasa-Koyama <tkr0rempe1230@gmail.com>
2025-12-15 10:55:32 +0900

    回転方向を変更

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 while (true) {
     if (Tinybit.Line_Sensor(Tinybit.enPos.LeftState, Tinybit.enLineState.Black) && Tinybit.Line_Sensor(Tinybit.enPos.RightState, Tinybit.enLineState.Black)) {
-        Tinybit.car_sport(0, 62)
+        Tinybit.car_sport(62, 0)
     } else if (Tinybit.Line_Sensor(Tinybit.enPos.RightState, Tinybit.enLineState.Black)) {
         Tinybit.car_sport(65, 65)
     } else if (Tinybit.Line_Sensor(Tinybit.enPos.LeftState, Tinybit.enLineState.Black)) {
@@ -12,7 +12,7 @@ while (true) {
             . . # . .
             `)
     } else if (Tinybit.Line_Sensor(Tinybit.enPos.LeftState, Tinybit.enLineState.White) && Tinybit.Line_Sensor(Tinybit.enPos.RightState, Tinybit.enLineState.White)) {
-        Tinybit.car_sport(65, 0)
+        Tinybit.car_sport(0, 62)
     } else {
         basic.showLeds(`
             . . # . .
