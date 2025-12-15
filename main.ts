c71eae4f1305d334db3038e370d6cde6e93d9bda
Tsukasa-Koyama <tkr0rempe1230@gmail.com>
2025-12-15 11:36:36 +0900

    線の太さ<2cm用

--- main.ts
+++ main.ts
@@ -1,12 +1,13 @@
 while (true) {
     if (Tinybit.Line_Sensor(Tinybit.enPos.LeftState, Tinybit.enLineState.Black) && Tinybit.Line_Sensor(Tinybit.enPos.RightState, Tinybit.enLineState.Black)) {
-        Tinybit.car_sport(62, 0)
+        Tinybit.car_sport(0, 0)
+        music.play(music.tonePlayable(262, music.beat(BeatFraction.Breve)), music.PlaybackMode.UntilDone)
     } else if (Tinybit.Line_Sensor(Tinybit.enPos.RightState, Tinybit.enLineState.Black)) {
-        Tinybit.car_sport(65, 65)
+        Tinybit.car_sport(65, 0)
     } else if (Tinybit.Line_Sensor(Tinybit.enPos.LeftState, Tinybit.enLineState.Black)) {
-        Tinybit.car_sport(0, 62)
+        Tinybit.car_sport(0, 65)
     } else if (Tinybit.Line_Sensor(Tinybit.enPos.LeftState, Tinybit.enLineState.White) && Tinybit.Line_Sensor(Tinybit.enPos.RightState, Tinybit.enLineState.White)) {
-        Tinybit.car_sport(62, 0)
+        Tinybit.car_sport(55, 55)
     } else {
         basic.showLeds(`
             . . # . .
