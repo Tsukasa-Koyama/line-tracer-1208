70d6e64383059cfaf61ae6cba317a2aa202c4d3a
Tsukasa-Koyama <tkr0rempe1230@gmail.com>
2025-12-15 21:45:43 +0900

    ボタンによる走行開始/停止の論理構造を変更

--- main.py
+++ main.py
@@ -1,51 +1,66 @@
+def on_button_pressed_a():
+    global button_state
+    button_state = True
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_b():
+    global button_state
+    button_state = False
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+button_state = False
+button_state = False
 while True:
-    if Tinybit.Line_Sensor(Tinybit.enPos.LEFT_STATE, Tinybit.enLineState.BLACK) and Tinybit.Line_Sensor(Tinybit.enPos.RIGHT_STATE, Tinybit.enLineState.BLACK):
-        Tinybit.car_sport(90, 90)
-        basic.show_leds("""
-            . . # . .
-            . . # . .
-            . . # . .
-            . . # . .
-            . . # . .
-            """)
-    elif Tinybit.Line_Sensor(Tinybit.enPos.RIGHT_STATE, Tinybit.enLineState.BLACK):
-        Tinybit.car_sport(65, 0)
-        basic.show_leds("""
-            . . # . .
-            . . # . .
-            # # # . .
-            . . # . .
-            . . # . .
-            """)
-    elif Tinybit.Line_Sensor(Tinybit.enPos.LEFT_STATE, Tinybit.enLineState.BLACK):
-        Tinybit.car_sport(0, 65)
-        basic.show_leds("""
-            . . # . .
-            . . # . .
-            . . # # #
-            . . # . .
-            . . # . .
-            """)
-    elif Tinybit.Line_Sensor(Tinybit.enPos.LEFT_STATE, Tinybit.enLineState.WHITE) and Tinybit.Line_Sensor(Tinybit.enPos.RIGHT_STATE, Tinybit.enLineState.WHITE):
-        Tinybit.car_sport(50, 50)
-        basic.show_leds("""
-            . . . . .
-            . . . . .
-            # # # # #
-            . . . . .
-            . . . . .
-            """)
-    else:
-        Tinybit.car_sport(0, 0)
-        music.play(music.tone_playable(262, music.beat(BeatFraction.BREVE)),
-            music.PlaybackMode.UNTIL_DONE)
-        basic.show_leds("""
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            """)
+    while button_state:
+        if Tinybit.Line_Sensor(Tinybit.enPos.LEFT_STATE, Tinybit.enLineState.BLACK) and Tinybit.Line_Sensor(Tinybit.enPos.RIGHT_STATE, Tinybit.enLineState.BLACK):
+            Tinybit.car_sport(90, 90)
+            basic.show_leds("""
+                . . # . .
+                . . # . .
+                . . # . .
+                . . # . .
+                . . # . .
+                """)
+        elif Tinybit.Line_Sensor(Tinybit.enPos.RIGHT_STATE, Tinybit.enLineState.BLACK):
+            Tinybit.car_sport(65, 0)
+            basic.show_leds("""
+                . . # . .
+                . . # . .
+                # # # . .
+                . . # . .
+                . . # . .
+                """)
+        elif Tinybit.Line_Sensor(Tinybit.enPos.LEFT_STATE, Tinybit.enLineState.BLACK):
+            Tinybit.car_sport(0, 65)
+            basic.show_leds("""
+                . . # . .
+                . . # . .
+                . . # # #
+                . . # . .
+                . . # . .
+                """)
+        elif Tinybit.Line_Sensor(Tinybit.enPos.LEFT_STATE, Tinybit.enLineState.WHITE) and Tinybit.Line_Sensor(Tinybit.enPos.RIGHT_STATE, Tinybit.enLineState.WHITE):
+            Tinybit.car_sport(50, 50)
+            basic.show_leds("""
+                . . . . .
+                . . . . .
+                # # # # #
+                . . . . .
+                . . . . .
+                """)
+        else:
+            Tinybit.car_sport(0, 0)
+            music.play(music.tone_playable(262, music.beat(BeatFraction.BREVE)),
+                music.PlaybackMode.IN_BACKGROUND)
+            basic.show_leds("""
+                # # # # #
+                # # # # #
+                # # # # #
+                # # # # #
+                # # # # #
+                """)
+    Tinybit.car_sport(0, 0)
+    basic.show_string("P")
 
 def on_forever():
     pass
--- main.ts
+++ main.ts
@@ -7,58 +7,59 @@ input.onButtonPressed(Button.B, function () {
 let button_state = false
 button_state = false
 while (true) {
-    if (button_state) {
-        if (Tinybit.Line_Sensor(Tinybit.enPos.LeftState, Tinybit.enLineState.Black) && Tinybit.Line_Sensor(Tinybit.enPos.RightState, Tinybit.enLineState.Black)) {
-            Tinybit.car_sport(90, 90)
-            basic.showLeds(`
-                . . # . .
-                . . # . .
-                . . # . .
-                . . # . .
-                . . # . .
-                `)
-        } else if (Tinybit.Line_Sensor(Tinybit.enPos.RightState, Tinybit.enLineState.Black)) {
-            Tinybit.car_sport(65, 0)
-            basic.showLeds(`
-                . . # . .
-                . . # . .
-                # # # . .
-                . . # . .
-                . . # . .
-                `)
-        } else if (Tinybit.Line_Sensor(Tinybit.enPos.LeftState, Tinybit.enLineState.Black)) {
-            Tinybit.car_sport(0, 65)
-            basic.showLeds(`
-                . . # . .
-                . . # . .
-                . . # # #
-                . . # . .
-                . . # . .
-                `)
-        } else if (Tinybit.Line_Sensor(Tinybit.enPos.LeftState, Tinybit.enLineState.White) && Tinybit.Line_Sensor(Tinybit.enPos.RightState, Tinybit.enLineState.White)) {
-            Tinybit.car_sport(50, 50)
-            basic.showLeds(`
-                . . . . .
-                . . . . .
-                # # # # #
-                . . . . .
-                . . . . .
-                `)
-        } else {
-            Tinybit.car_sport(0, 0)
-            music.play(music.tonePlayable(262, music.beat(BeatFraction.Breve)), music.PlaybackMode.UntilDone)
-            basic.showLeds(`
-                # # # # #
-                # # # # #
-                # # # # #
-                # # # # #
-                # # # # #
-                `)
+    while (button_state) {
+        while (true) {
+            if (Tinybit.Line_Sensor(Tinybit.enPos.LeftState, Tinybit.enLineState.Black) && Tinybit.Line_Sensor(Tinybit.enPos.RightState, Tinybit.enLineState.Black)) {
+                Tinybit.car_sport(90, 90)
+                basic.showLeds(`
+                    . . # . .
+                    . . # . .
+                    . . # . .
+                    . . # . .
+                    . . # . .
+                    `)
+            } else if (Tinybit.Line_Sensor(Tinybit.enPos.RightState, Tinybit.enLineState.Black)) {
+                Tinybit.car_sport(65, 0)
+                basic.showLeds(`
+                    . . # . .
+                    . . # . .
+                    # # # . .
+                    . . # . .
+                    . . # . .
+                    `)
+            } else if (Tinybit.Line_Sensor(Tinybit.enPos.LeftState, Tinybit.enLineState.Black)) {
+                Tinybit.car_sport(0, 65)
+                basic.showLeds(`
+                    . . # . .
+                    . . # . .
+                    . . # # #
+                    . . # . .
+                    . . # . .
+                    `)
+            } else if (Tinybit.Line_Sensor(Tinybit.enPos.LeftState, Tinybit.enLineState.White) && Tinybit.Line_Sensor(Tinybit.enPos.RightState, Tinybit.enLineState.White)) {
+                Tinybit.car_sport(50, 50)
+                basic.showLeds(`
+                    . . . . .
+                    . . . . .
+                    # # # # #
+                    . . . . .
+                    . . . . .
+                    `)
+            } else {
+                Tinybit.car_sport(0, 0)
+                music.play(music.tonePlayable(262, music.beat(BeatFraction.Breve)), music.PlaybackMode.InBackground)
+                basic.showLeds(`
+                    # # # # #
+                    # # # # #
+                    # # # # #
+                    # # # # #
+                    # # # # #
+                    `)
+            }
         }
-    } else {
-        Tinybit.car_sport(0, 0)
-        basic.showString("PAUSED")
     }
+    Tinybit.car_sport(0, 0)
+    basic.showString("P")
 }
 basic.forever(function () {
 	
