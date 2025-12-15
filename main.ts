1488a0466df1ee8316e6337239160066cd81fee7
Tsukasa-Koyama <tkr0rempe1230@gmail.com>
2025-12-15 20:43:01 +0900

    Restore '2025/12/15 11:55:26 線幅3cm　直進速度を上げた'

--- main.ts
+++ main.ts
@@ -1,49 +1,21 @@
 while (true) {
     if (Tinybit.Line_Sensor(Tinybit.enPos.LeftState, Tinybit.enLineState.Black) && Tinybit.Line_Sensor(Tinybit.enPos.RightState, Tinybit.enLineState.Black)) {
         Tinybit.car_sport(90, 90)
-        basic.showLeds(`
-            . . # . .
-            . . # . .
-            . . # . .
-            . . # . .
-            . . # . .
-            `)
     } else if (Tinybit.Line_Sensor(Tinybit.enPos.RightState, Tinybit.enLineState.Black)) {
         Tinybit.car_sport(65, 0)
-        basic.showLeds(`
-            . . # . .
-            . . # . .
-            # # # . .
-            . . # . .
-            . . # . .
-            `)
     } else if (Tinybit.Line_Sensor(Tinybit.enPos.LeftState, Tinybit.enLineState.Black)) {
         Tinybit.car_sport(0, 65)
-        basic.showLeds(`
-            . . # . .
-            . . # . .
-            . . # # #
-            . . # . .
-            . . # . .
-            `)
     } else if (Tinybit.Line_Sensor(Tinybit.enPos.LeftState, Tinybit.enLineState.White) && Tinybit.Line_Sensor(Tinybit.enPos.RightState, Tinybit.enLineState.White)) {
         Tinybit.car_sport(50, 50)
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            # # # # #
-            . . . . .
-            . . . . .
-            `)
     } else {
         Tinybit.car_sport(0, 0)
         music.play(music.tonePlayable(262, music.beat(BeatFraction.Breve)), music.PlaybackMode.UntilDone)
         basic.showLeds(`
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
+            . . # . .
+            . . # . .
+            . . # . .
+            . . . . .
+            . . # . .
             `)
     }
 }
