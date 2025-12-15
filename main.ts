e933ef7eb8a7df92f022ca1ca01d2954e49626fb
Tsukasa-Koyama <tkr0rempe1230@gmail.com>
2025-12-15 20:40:26 +0900

    各条件に対して、固有のLED表示を追加

--- main.ts
+++ main.ts
@@ -1,21 +1,49 @@
 while (true) {
     if (Tinybit.Line_Sensor(Tinybit.enPos.LeftState, Tinybit.enLineState.Black) && Tinybit.Line_Sensor(Tinybit.enPos.RightState, Tinybit.enLineState.Black)) {
         Tinybit.car_sport(90, 90)
-    } else if (Tinybit.Line_Sensor(Tinybit.enPos.RightState, Tinybit.enLineState.Black)) {
-        Tinybit.car_sport(65, 0)
-    } else if (Tinybit.Line_Sensor(Tinybit.enPos.LeftState, Tinybit.enLineState.Black)) {
-        Tinybit.car_sport(0, 65)
-    } else if (Tinybit.Line_Sensor(Tinybit.enPos.LeftState, Tinybit.enLineState.White) && Tinybit.Line_Sensor(Tinybit.enPos.RightState, Tinybit.enLineState.White)) {
-        Tinybit.car_sport(50, 50)
-    } else {
-        Tinybit.car_sport(0, 0)
-        music.play(music.tonePlayable(262, music.beat(BeatFraction.Breve)), music.PlaybackMode.UntilDone)
         basic.showLeds(`
             . . # . .
             . . # . .
             . . # . .
-            . . . . .
             . . # . .
+            . . # . .
+            `)
+    } else if (Tinybit.Line_Sensor(Tinybit.enPos.RightState, Tinybit.enLineState.Black)) {
+        Tinybit.car_sport(65, 0)
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # # # . .
+            . . # . .
+            . . # . .
+            `)
+    } else if (Tinybit.Line_Sensor(Tinybit.enPos.LeftState, Tinybit.enLineState.Black)) {
+        Tinybit.car_sport(0, 65)
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            . . # # #
+            . . # . .
+            . . # . .
+            `)
+    } else if (Tinybit.Line_Sensor(Tinybit.enPos.LeftState, Tinybit.enLineState.White) && Tinybit.Line_Sensor(Tinybit.enPos.RightState, Tinybit.enLineState.White)) {
+        Tinybit.car_sport(50, 50)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # # # # #
+            . . . . .
+            . . . . .
+            `)
+    } else {
+        Tinybit.car_sport(0, 0)
+        music.play(music.tonePlayable(262, music.beat(BeatFraction.Breve)), music.PlaybackMode.UntilDone)
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
             `)
     }
 }
