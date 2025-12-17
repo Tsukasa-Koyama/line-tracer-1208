f5c6edd725808f47881493cdf9de33ae6279b630
Tsukasa-Koyama <tkr0rempe1230@gmail.com>
2025-12-17 15:28:50 +0900

    while trueを消去

--- main.ts
+++ main.ts
@@ -6,61 +6,56 @@ input.onButtonPressed(Button.B, function () {
 })
 let button_state = false
 button_state = false
-while (true) {
+basic.forever(function () {
     while (button_state) {
-        while (true) {
-            if (Tinybit.Line_Sensor(Tinybit.enPos.LeftState, Tinybit.enLineState.Black) && Tinybit.Line_Sensor(Tinybit.enPos.RightState, Tinybit.enLineState.Black)) {
-                Tinybit.car_sport(90, 90)
-                basic.showLeds(`
-                    . . # . .
-                    . . # . .
-                    . . # . .
-                    . . # . .
-                    . . # . .
-                    `)
-            } else if (Tinybit.Line_Sensor(Tinybit.enPos.RightState, Tinybit.enLineState.Black)) {
-                Tinybit.car_sport(65, 0)
-                basic.showLeds(`
-                    . . # . .
-                    . . # . .
-                    # # # . .
-                    . . # . .
-                    . . # . .
-                    `)
-            } else if (Tinybit.Line_Sensor(Tinybit.enPos.LeftState, Tinybit.enLineState.Black)) {
-                Tinybit.car_sport(0, 65)
-                basic.showLeds(`
-                    . . # . .
-                    . . # . .
-                    . . # # #
-                    . . # . .
-                    . . # . .
-                    `)
-            } else if (Tinybit.Line_Sensor(Tinybit.enPos.LeftState, Tinybit.enLineState.White) && Tinybit.Line_Sensor(Tinybit.enPos.RightState, Tinybit.enLineState.White)) {
-                Tinybit.car_sport(50, 50)
-                basic.showLeds(`
-                    . . . . .
-                    . . . . .
-                    # # # # #
-                    . . . . .
-                    . . . . .
-                    `)
-            } else {
-                Tinybit.car_sport(0, 0)
-                music.play(music.tonePlayable(262, music.beat(BeatFraction.Breve)), music.PlaybackMode.InBackground)
-                basic.showLeds(`
-                    # # # # #
-                    # # # # #
-                    # # # # #
-                    # # # # #
-                    # # # # #
-                    `)
-            }
+        if (Tinybit.Line_Sensor(Tinybit.enPos.LeftState, Tinybit.enLineState.Black) && Tinybit.Line_Sensor(Tinybit.enPos.RightState, Tinybit.enLineState.Black)) {
+            Tinybit.car_sport(90, 90)
+            basic.showLeds(`
+                . . # . .
+                . . # . .
+                . . # . .
+                . . # . .
+                . . # . .
+                `)
+        } else if (Tinybit.Line_Sensor(Tinybit.enPos.RightState, Tinybit.enLineState.Black)) {
+            Tinybit.car_sport(65, 0)
+            basic.showLeds(`
+                . . # . .
+                . . # . .
+                # # # . .
+                . . # . .
+                . . # . .
+                `)
+        } else if (Tinybit.Line_Sensor(Tinybit.enPos.LeftState, Tinybit.enLineState.Black)) {
+            Tinybit.car_sport(0, 65)
+            basic.showLeds(`
+                . . # . .
+                . . # . .
+                . . # # #
+                . . # . .
+                . . # . .
+                `)
+        } else if (Tinybit.Line_Sensor(Tinybit.enPos.LeftState, Tinybit.enLineState.White) && Tinybit.Line_Sensor(Tinybit.enPos.RightState, Tinybit.enLineState.White)) {
+            Tinybit.car_sport(50, 50)
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                # # # # #
+                . . . . .
+                . . . . .
+                `)
+        } else {
+            Tinybit.car_sport(0, 0)
+            music.play(music.tonePlayable(262, music.beat(BeatFraction.Breve)), music.PlaybackMode.InBackground)
+            basic.showLeds(`
+                # # # # #
+                # # # # #
+                # # # # #
+                # # # # #
+                # # # # #
+                `)
         }
     }
     Tinybit.car_sport(0, 0)
     basic.showString("P")
-}
-basic.forever(function () {
-	
 })
